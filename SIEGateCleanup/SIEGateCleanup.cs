8ccaa789cbc101bb5b00e03a5662ad714ab25da0
jeffh <jeffh@nojapower.com.au>
2019-06-10 12:12:47 +1000

    Added statiscal information to log

--- SIEGateCleanup/SIEGateCleanup.cs
+++ SIEGateCleanup/SIEGateCleanup.cs
@@ -9,15 +9,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using System.IO;
+using System.Collections;
+using System.Collections.Specialized;
 
 namespace SIEGateCleanup
 {
     public partial class SIEGateCleanup : ServiceBase
     {
         private static readonly Logger.Logger _log = Logger.Log.GetInstance("SIEGateCleanup");
+        private static DateTime upTime = DateTime.Now;
+        OrderedDictionary _stats = new OrderedDictionary();
 
         private static Timer aTimer;
         private string _path = null;
+
+        class CleanupStats
+        {
+            public long totalBytes;
+            public int totalFiles;
+        }
 
         public void RunAsConsole(string[] args)
         {
@@ -56,39 +66,70 @@ namespace SIEGateCleanup
         {
             try
             {
-                Console.WriteLine(String.Format("{0:yyyy-MM-dd HH:mm:ss} : Cleaning...", e.SignalTime));
+                _log.Info("Thread started");
                 // after initial execution, set timer to its repeating state
                 aTimer.Interval = 60 * 1000;
 
-                PurgeFiles(_path);
+                CleanupStats stat = PurgeFiles(_path);
+                _stats.Add(DateTime.Now, stat);
+
+                PrintStats();
+                //_log.Info(String.Format("Summary: Files deleted {0}, Total bytes {1:n0}mb", stats.totalFiles, stats.totalBytes / (1024 * 1024)));
             }
             catch
             {
             }
         }
 
-        private void PurgeFiles(string dirPath)
+        private void PrintStats()
         {
+            _log.Info(String.Format("Summary as of {0}", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")));
+            _log.Info(String.Format("{0,-25}{1,11}{2,15}", "Time", "Total Files","Total Mb"));
+            foreach ( DateTime item in _stats.Keys)
+            {
+                CleanupStats currentStat = (CleanupStats)_stats[item];
+                _log.Info(String.Format("{0,-25}{1,11}{2,15:n0} Mb", item.ToString("dd/MM/yyyy HH:mm:ss"), currentStat.totalFiles, currentStat.totalBytes / (1024 * 1024)));
+            }
+            _log.Info("END");
+        }
+
+        private CleanupStats PurgeFiles(string dirPath)
+        {
+            CleanupStats stats = new CleanupStats() { totalBytes = 0, totalFiles = 0 };
+
             try
             {
-                string[] folders = Directory.GetDirectories(dirPath);
+                string[] folders = Directory.GetDirectories(dirPath, "*", SearchOption.AllDirectories);
 
                 foreach (string folder in folders)
                 {
-                    string[] files = Directory.GetFiles(folder, "*", SearchOption.AllDirectories);
+                    string[] files = Directory.GetFiles(folder, "*", SearchOption.TopDirectoryOnly);//.AllDirectories);
+                    if ( files.Length > 0)
+                        _log.Info(String.Format("Purging folder {0} -> {1} files", folder, files.Length));
 
                     foreach (string file in files)
                     {
-                        //    File.Delete(file);
-                        _log.Info(file);
-                    //    Console.WriteLine("Deleting " + file);
+                        try
+                        {
+                            FileInfo fi = new FileInfo(file);
+                            stats.totalBytes += fi.Length;
+                            stats.totalFiles++;
+                            //    File.Delete(file);
+                //            _log.Info(file);
+                            //    Console.WriteLine("Deleting " + file);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Inner loop: " + ex.Message);
+                        }
                     }
                 }
             }
             catch ( Exception ex)
             {
-
+                Console.WriteLine("Outer loop: " + ex.Message);
             }
+            return stats;
         }
 
         public void StopProcess()
